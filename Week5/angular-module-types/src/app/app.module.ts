32661af9279a8d7ae7e39f21146b4bbf9066c4cf
Niteesh29 <niteesh.mishra@crownstack.com>
2022-05-06 14:25:46 +0530

    created angular-module-type

---
+++ Week5/angular-module-types/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    //inside declaration you will put components , directives and custom pipe
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
