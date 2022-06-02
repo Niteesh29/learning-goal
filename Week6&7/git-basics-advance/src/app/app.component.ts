b2c5e9d52c743f03afb086fa0320b06bdacdd525
Niteesh29 <niteesh.mishra@crownstack.com>
2022-06-02 18:15:48 +0530

    commit of new_branch

--- Week6&7/git-basics-advance/src/app/app.component.ts
+++ Week6&7/git-basics-advance/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { nextTick } from 'process';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'git-basics-advance';
+
+  ngOnInit(): void {
+    const obsevable = new Observable((sub) => {
+      setTimeout(() => {
+        sub.next('observable is working')
+      }, 1000)
+    }).subscribe((res) => {
+      console.log(res)
+    })
+  }
 }
